eb1fd0881e4512476a49fabfd173de4987057f49
WooYong Kim <wykim970@gmail.com>
2021-08-01 16:58:51 +0900

    Part02 chapter03 08 finished

--- app/src/main/java/org/techtown/androidstudy/MainActivity.java
+++ app/src/main/java/org/techtown/androidstudy/MainActivity.java
@@ -3,12 +3,15 @@ package org.techtown.androidstudy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
+    GestureDetector detector;
 
     public void println(String data) {
         textView.append(data+"\n");
@@ -40,6 +43,59 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        detector = new GestureDetector(this, new GestureDetector.OnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent motionEvent) {
+                println("onDown 호출됨");
+                return true;
+            }
+
+            @Override
+            public void onShowPress(MotionEvent motionEvent) {
+
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent motionEvent) {
+                return false;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+                return false;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent motionEvent) {
+                println("onLongPress 호출됨");
+            }
+
+            @Override
+            public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+                println("onFling 호출됨" + v + ", " + v1);
+                return true;
+            }
+        });
+
+        View view2 = findViewById(R.id.view2);
+        view2.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                detector.onTouchEvent(event);
+                return true;
+            }
+        });
     }
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {     // 키가 눌렸을 때 자동으로 호출, 어떤 키가 눌렸는지 구분
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            println("시스템 [BACK] 버튼 눌림.");
+            return true;  // 이후 동작이 취소됨
+        }
+        return false;
+    }
+
+    // 단말 방향이 바뀌는 경우
 }
