c14192557db6335d1601810f3ec62109107bb552
WooYong Kim <wykim970@gmail.com>
2021-08-03 23:57:59 +0900

    ~Part02 chapter04 06 finished

---
+++ app/src/main/java/org/techtown/androidstudy/Parcelable2Activity.java
@@ -0,0 +1,40 @@
+package org.techtown.androidstudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Parcelable2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parcelable2);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        Intent intent = getIntent();
+        processIntent(intent);
+    }
+
+    public void processIntent(Intent intent) {
+        if (intent != null) {
+            Bundle bundle = intent.getExtras();
+            SimpleData data = bundle.getParcelable("data");
+            if (data != null) {
+                Toast.makeText(this,"전달받은 객체 : " + data.code + ", " + data.message, Toast.LENGTH_LONG).show();
+            }
+
+        }
+    }
+}
