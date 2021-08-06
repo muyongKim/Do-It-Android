a31f77f165728e0d2faa6429e2fa4e8da43d11ec
WooYong Kim <wykim970@gmail.com>
2021-08-06 22:13:28 +0900

    ~Part02 chapter05 07 finished

---
+++ app/src/main/java/org/techtown/androidstudy/OptionActivity.java
@@ -0,0 +1,54 @@
+package org.techtown.androidstudy;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class OptionActivity extends AppCompatActivity {
+    ActionBar actionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_option);
+
+        actionBar = getSupportActionBar();
+        actionBar.setLogo(R.drawable.picture1);
+        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME|ActionBar.DISPLAY_USE_LOGO);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int curId = item.getItemId();
+
+        switch (curId) {
+            case R.id.menu_refresh:
+                showToast("새로고침 메뉴 선택됨");
+                break;
+            case R.id.menu_search:
+                showToast("검색 메뉴 선택됨");
+                break;
+            case R.id.menu_settings:
+                showToast("설정 메뉴 선택됨");
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void showToast(String data) {
+        Toast.makeText(this, data, Toast.LENGTH_LONG).show();
+    }
+}
