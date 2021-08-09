8e30877d5aee52fea23f4059d96455f75049a2d2
WooYong Kim <wykim970@gmail.com>
2021-08-09 23:40:20 +0900

    ~Part02 chapter07 01 finished

---
+++ app/src/main/java/org/techtown/androidstudy/NinePatchActivity.java
@@ -0,0 +1,14 @@
+package org.techtown.androidstudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NinePatchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nine_patch);
+    }
+}
