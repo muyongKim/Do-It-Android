40f4c8fd4ae2013db6a77da5d27bc710d0e8d0bc
WooYong Kim <wykim970@gmail.com>
2021-08-12 23:50:56 +0900

    ~Part02 chapter09 01 finished

---
+++ app/src/main/java/org/techtown/androidstudy/ThreadActivity.java
@@ -0,0 +1,47 @@
+package org.techtown.androidstudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ThreadActivity extends AppCompatActivity {
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thread);
+
+        textView = findViewById(R.id.textView);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BackgroundThread thread = new BackgroundThread();
+                thread.start();
+            }
+        });
+    }
+
+    class BackgroundThread extends Thread {
+        int value = 0;
+
+        public void run() {
+            for (int i = 0; i < 100; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch(Exception e) {}
+
+                value += 1;
+                Log.d("MyThread", "value : " + value);
+
+                textView.setText("값 : " + value);
+            }
+        }
+    }
+}
