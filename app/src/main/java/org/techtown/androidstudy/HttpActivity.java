ff26e713d95fbeb06b98e666e504b78b4c571b76
WooYong Kim <wykim970@gmail.com>
2021-08-16 21:37:49 +0900

    ~Part02 chapter10 04 finished

---
+++ app/src/main/java/org/techtown/androidstudy/HttpActivity.java
@@ -0,0 +1,15 @@
+package org.techtown.androidstudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+// 21.08.16 Http
+public class HttpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_http);
+    }
+}
