27d09dee5e57c4477311d0283318a5720fa08115
WooYong Kim <wykim970@gmail.com>
2021-08-18 15:59:09 +0900

    ~Part02 chapter10 05 finished

--- app/src/main/java/org/techtown/androidstudy/HttpActivity.java
+++ app/src/main/java/org/techtown/androidstudy/HttpActivity.java
@@ -3,13 +3,87 @@ package org.techtown.androidstudy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 // 21.08.16 Http
 public class HttpActivity extends AppCompatActivity {
+    EditText editText;
+    TextView textView;
+
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_http);
+
+        editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.textView);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String urlStr = editText.getText().toString();
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        request(urlStr);
+                    }
+                }).start();
+            }
+        });
+    }
+
+    public void request(String urlStr) {
+        try {
+            StringBuilder builder = new StringBuilder();
+
+            URL url = new URL(urlStr);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            if (conn != null) {
+                conn.setConnectTimeout(10000);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+
+                int resCode = conn.getResponseCode();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                String line = null;
+                while(true) {
+                    line = reader.readLine();
+                    if (line == null)
+                        break;
+                    builder.append(line+"\n");
+                }
+                reader.close();
+                conn.disconnect();
+            }
+            println("응답 : " + builder.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void println(String data) {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                textView.append(data+"\n");
+            }
+        });
     }
 }
